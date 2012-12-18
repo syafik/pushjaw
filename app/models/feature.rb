8d44132fa35544ba57ac38d94d25be967f3525b8
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2012-12-18 16:22:34 +0700

    last edit

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,4 @@
 class Feature < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-
+  attr_accessible :content, :name, :title, :picture
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,6 @@
 class Image < ActiveRecord::Base
   # attr_accessible :title, :body
-  belongs_to :imageable, :polymorphic => true
-  attr_accessible :image, :imageable_id, :imageable_type
+  attr_accessible :image, :service_id, :portfolio_id
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  
+  belongs_to :service
 end
