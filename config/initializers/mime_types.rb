2a7ccd7df7c0979ffdb8bd358c21d4c467587e36
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2012-12-20 14:23:32 +0700

    add mime

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# register MIME type with Rails 
+Mime::Type.register "video/mp4", :m4v
+
+# register MIME type with MIME::Type gem 
+MIME::Types.add(MIME::Type.from_array("video/mp4", %(m4v)))
