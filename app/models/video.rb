cc8babcc02865e5dcb9d23049c8b4268450d748b
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2012-12-20 16:55:27 +0700

    edit video.rb

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,6 @@
 class Video < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :video
-  belongs_to :home
   has_attached_file :video, :styles => { 
     :medium => { :geometry => "640x480", :format => 'mp4' },
     :medium => { :geometry => "640x480", :format => 'flv' }
