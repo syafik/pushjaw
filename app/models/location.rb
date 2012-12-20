929f22c3ae8f7e1fcfdae39d007707c344f9a3a1
adika <adika.ngandro@gmail.com>
2012-12-20 13:58:29 +0700

    edited model/location

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,11 @@ class Location < ActiveRecord::Base
   acts_as_mappable
   before_validation :geocode_address, :on => :create
 
+  private
+  def geocode_address
+    geo=Geokit::Geocoders::MultiGeocoder.geocode ('jln mig 3 cimahi')
+    errors.add(:address, "Could not Geocode address") if !geo.success
+    self.lat, self.lng = geo.lat,geo.lng if geo.success
+  end
+  
 end
