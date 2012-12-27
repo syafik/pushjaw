c76d00da68aab0188139920a110362a9e3843a87
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2012-12-27 12:28:52 +0700

    video edit

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,25 @@
 class Video < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :video
+  attr_accessible :video, :title, :image_id
+  has_one :image, :dependent => :destroy, :inverse_of => :video
+
+  # for nested fields: (natural choice for a has_one association)
+
+    attr_accessible :image_attributes
+    accepts_nested_attributes_for :image, :allow_destroy => true
+
+  # or if you want a dropdown select: 
+
+    attr_accessible :image_id
+
+    # Since ActiveRecord does not create setters/getters for has_one associations (why is beyond me), diy:
+    def image_id
+      self.image.try :id
+    end
+    def image_id=(id)
+      self.image = Image.find_by_id(id)
+    end
+  
   has_attached_file :video, :styles => { 
     :medium => { :geometry => "640x480", :format => 'mp4' },
     :medium => { :geometry => "640x480", :format => 'flv' }
