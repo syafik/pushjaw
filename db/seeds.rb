dc0adfbd268fbda467ad91d4fc84d69139a8e463
adika <adika.ngandro@gmail.com>
2013-01-07 09:23:32 +0700

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Product.delete_all
-About.create(content: "Save videos to watch later, watch recommendations just for you, or subscribe to get ... See videos shared by your friends across all your social networks — all in ...", active: true)
-About.create(content: "watch later, watchs shared by your friends across all your social networks — all in ...", active: false)
+About.delete_all
+About.create(:content => "Save videos to watch later, watch recommendations just for you, or subscribe to get ... See videos shared by your friends across all your social networks",
+  :active => true )
+About.create(:content => "watch later, watchs shared by your friends across all your social networks",
+  :active => false )
 
 Comment.delete_all
 Comment.create(name: "Andreas Kisserk", title: "Developer in StablesConfusion", address: "CA", comment: "Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum has been the industry's standard dummy text ever 1500s, when an unknown printer took a galley of type and scrammake a type specimen book..", active: true)
 Comment.create(name: "Dani", title: "Developer in Cijagra", address: "INA", comment: "Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum has been the industry's standard dummy text ever 1500s, when an unknown printer took a galley of type and scrammake a type specimen book..", active: false)\
   
 Location.delete_all
-Location.create(address: "jln mig 3 no 1", mobile_phone: "+6281809466884", mobile: "+622260001425", city: "Cimahi, West Java - Indonesia")
+Location.create(address: "jln mig 3 no 1", mobile_phone: "+6281809466884", phone: "+622260001425", city: "Cimahi, West Java - Indonesia")
 
 Team.delete_all
 Image.delete_all
@@ -32,7 +26,7 @@ User.delete_all
 User.create(email: 'pushjaw@pushjaw.com', password: 'pushjaw123456')
 
 
-
+Home.delete_all
 print 'seeding database'
 10.times do |home|
   home = Home.create :content => "Lorem Ipsum is simply dummy text of the 
@@ -44,6 +38,7 @@ print 'seeding database'
   print '.'
 end
 
+Service.delete_all
 10.times do |service|
   service = Service.create!(:name => "Web Developer",
     :content => "Lorem Ipsum is simply dummy text of the 
@@ -57,6 +52,7 @@ end
   Image.create!(:service_id => service.id, :image => open(Rails.root.join('app/assets/images/pushjawcompany.png')))
 end
 
+Portfolio.delete_all
 10.times do |portfolio|
   portfolio = Portfolio.create!(:name => "Pushjaw",
     :content => "Pushjaw",
@@ -67,6 +63,7 @@ end
   Image.create!(:portfolio_id => portfolio.id, :image => open(Rails.root.join('app/assets/images/pushjawcompany.png')))  
 end
 
+Blog.delete_all
 10.times do |blog|
   blog = Blog.create!(:content => "Pushjaw",
                       :title => "Pushjaw",
@@ -74,6 +71,7 @@ end
     print '.'
 end
 
+Video.delete_all
 3.times do |video|
   video = Video.create!(
     :description => "Pushjaw",
