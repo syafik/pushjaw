076e22121f80f2e1c2bd6fdaa6e11f5d5497092f
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2013-01-10 11:55:01 +0700

    add authenication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-#  before_filter :authenticate
+  before_filter :authenticate
 
-#  protected
+  protected
 
-#  def authenticate
-#    authenticate_or_request_with_http_basic do |username, password|
-#      username == "pushjaw" && password == "pushjaw951478"
-#    end
-#  end
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "pushjaw" && password == "pushjaw951478"
+    end
+  end
 end
