26f1f70dac2198984f9c27ac3d67b86c219624f2
pushjaw <pushjaw@pushjaw-Satellite-U405.(none)>
2013-01-14 09:24:49 +0700

    install script sh

--- script.sh
+++ script.sh
@@ -1,1 +1,5 @@
-sudo apt-get install libmp3lame-dev ffmpeg
+sudo apt-get install ubuntu-restricted-extras
+sudo apt-get install imagemagick libmagickwand-dev
+sudo apt-get install libxslt-dev libxml2-dev
+sudo apt-get install libpq-dev
+sudo apt-get install libsqlite3-dev
