4bd5c8163859c694a09ec4f996cd816dd8236403
syafik <syafiklijtk04@gmail.com>
2013-03-25 11:32:42 +0700

    off http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   if Rails.env.eql?("production")
-    http_basic_authenticate_with :name => "pushjaw", :password => "pushjaw"
+    #http_basic_authenticate_with :name => "pushjaw", :password => "pushjaw"
   end
 end
