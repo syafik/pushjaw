56baac22fbfc974af24dd3dee1287bcca1385de6
syafik <syafiklijtk04@gmail.com>
2013-03-28 18:52:37 +0700

    update style size image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
 
   attr_accessible :image, :team_id, :portfolio_id, :service_id
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :service_thumb => "120x80>", :team_thumb => "89x105>" }
   belongs_to :service, :inverse_of => :images
   belongs_to :team, :inverse_of => :image
   belongs_to :portfolio, :inverse_of => :image
